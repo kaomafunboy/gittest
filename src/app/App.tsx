ddaf79a14bb9b76f4159bec2d4b6be824ccf7bdf
Nurdin Bakytbekov <kmeks81@gmail.com>
2024-04-07 20:09:33 +0600

    i've change h2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>lol</h1>
+        <h2>nurdin</h2>
     </div>
   );
 }
